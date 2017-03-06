f671ea2b517926a03b2b2ca61aacfcdbff619252
Life is Game <nobl11@bigmir.net>
2017-03-06 22:08:19 +0200

    - created new branch

---
+++ src/main/java/com/goit/domain/hibernate/HibernateSkillDAO.java
@@ -0,0 +1,46 @@
+package com.goit.domain.hibernate;
+
+import com.goit.domain.dao.SkillDAO;
+import com.goit.model.Skill;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
+/**
+ * Created by Den on 04-Mar-17.
+ */
+public class HibernateSkillDAO implements SkillDAO {
+
+    private SessionFactory sessionFactory;
+
+    @Override
+    public Skill find(Integer id) {
+        try(Session session = sessionFactory.openSession()) {
+            session.joinTransaction();
+            return session.find(Skill.class, id);
+        }
+    }
+
+    @Override
+    public List<Skill> findAll() {
+        return null;
+    }
+
+    @Override
+    public void delete(Integer skillId) {
+
+    }
+
+    @Override
+    public void save(Skill skill) {
+        Session session = sessionFactory.openSession();
+        session.joinTransaction();
+        session.persist(skill);
+        session.close();
+    }
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+}
---
+++ src/test/java/com/goit/domain/hibernate/HibernateSkillDAOTest.java
@@ -0,0 +1,42 @@
+package com.goit.domain.hibernate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.goit.domain.dao.SkillDAO;
+import com.goit.model.Skill;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.util.Assert;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/test-application-context.xml")
+public class HibernateSkillDAOTest {
+
+    @Autowired
+    private SkillDAO skillDAO;
+
+    @Autowired
+    private DataSourceTransactionManager txManager;
+
+    @Test
+    public void find() throws JsonProcessingException {
+        DefaultTransactionDefinition definition = new DefaultTransactionDefinition(TransactionDefinition.PROPAGATION_REQUIRED);
+        TransactionStatus transactionStatus = txManager.getTransaction(definition);
+
+        Skill skill = skillDAO.find(4);
+        ObjectMapper mapper = new ObjectMapper();
+        String s = mapper.writeValueAsString(skill);
+        Assert.notNull(skill);
+
+        txManager.commit(transactionStatus);
+    }
+
+}
